5e511b9a40b843eba011f43433e220792be649c0
linhao <linhao@gjzq.com.cn>
2022-02-20 20:40:35 +0800

    feat: 删除数据以及输入数字为两位数时，光标定位逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import {VersionInput} from './VersionInput';
 
 const App: FC = (): JSX.Element => {
   
+  const onVersionChange = (value: string) => {
+    console.log(value);
+  }
+
   return <div>
-    <VersionInput value='1.2.0'></VersionInput>
+    <VersionInput value='1.2.0' onChange={onVersionChange} />
   </div>
 }
 
--- src/VersionInput/VersionInput.tsx
+++ src/VersionInput/VersionInput.tsx
@@ -1,37 +1,75 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
 import { FC } from 'react';
 import './VersionInput.css'
 
+/**
+ * 1、如果传入value，将value分割成三份展示
+ * 2、如果有onChange事件，将value传给父组件
+ */
 type Props = {
-    value?: string;
+  value?: string;
+  onChange?: (version: string) => void,
 }
 
 export const VersionInput: FC<Props> = (props): JSX.Element => {
-    const [inputValues, setInputValues] = useState<string[]>();
-    const {value} = props;
+  const [inputValues, setInputValues] = useState<string[]>();
+  const inputRef: any = useRef();
+  const {value, onChange} = props;
 
-    const onInputChange = useCallback(() => {
+  const onInputChange = useCallback((e, index) => {
+    if (e.target.value.length === 2 && index < 2) {
+      const childInputWrapper = inputRef.current.children[index + 1];
+      childInputWrapper.children[0].focus();
+    }
+  }, [])
 
-    }, [])
+  const onInputKeyUp = useCallback((e, index) => {
+    if (+e.keyCode === 8 && !e.target.value && index > 0) { // 删除事件
+      const childInputWrapper = inputRef.current.children[index-1];
+      childInputWrapper.children[0].focus();
+    }
+  }, [])
 
-    useEffect(() => {
-        if (value && value.indexOf('.')) {
-            const values = value.split('.');
-            setInputValues(values);
-        }
-    }, [value])
+  const onInputBlur = useCallback(() => {
+    if (onChange) {
+      const childInputWrapper = Array.from(inputRef.current.children);
+      const result: string[] = [];
+      childInputWrapper.forEach((child: any) => {
+        const input = child['children'][0];
+        result.push(input.value);
+      })
+      if (result.length === 3) {
+        onChange(result.join('.'));
+      } else {
+        onChange('');
+      }
+    }
+  }, [])
 
-    return <div className='version-input'>
-        {inputValues?.map((value, index) => {
-            return <div className='version-input-content' key={`version-input-key${index}`}>
-                <input
-                    className='version-input-inner'
-                    type='text'
-                    value={value}
-                    onChange={onInputChange}
-                    maxLength={2} />
-                {index != 2 && <>.</>}
-            </div>
-        })}
-    </div> 
+  useEffect(() => {
+    if (value && value.indexOf('.')) {
+      const values = value.split('.');
+      setInputValues(values);
+    }
+  }, [value])
+
+  return <div
+    className='version-input'
+    onBlur={onInputBlur}
+    ref={inputRef}>
+    {inputValues?.map((value, index) => {
+      return <div
+        className='version-input-content'
+        key={`version-input-key${index}`}
+        onKeyUp={(e) => onInputKeyUp(e, index)}>
+        <input
+          className='version-input-inner'
+          type='text'
+          defaultValue={value}
+          onChange={(e) => onInputChange(e, index)}
+          maxLength={2} />
+        {index != 2 && <>.</>}
+      </div>
+    })}
+  </div> 
 }
